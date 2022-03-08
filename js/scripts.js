b7dc3899bf85fcfb4c30b1a2fdaeac876088d294
joey57 <njorogejoey5@gmail.com>
2022-03-08 11:32:52 +0300

    added click  function to p and show fucntion to img

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
- $('h1').click(function(){
-  alert("This is a header.");
-  alert("I told you, THIS IS A HEADER!");
- });
- $('P').click(function(){
-  alert("This is paragraph")
- });
- $('img').click(function(){
-  alert("This is an image")
- });
+  $("p").click(function(){
+    $("img").show();
+  });
 });
