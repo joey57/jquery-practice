24da402d4d207dcb983f20bc73d34c146a7610a3
joey57 <njorogejoey5@gmail.com>
2022-03-08 14:16:09 +0300

    added addclass and removeclass functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
-  $(".clickable").click(function(){
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
+  $("button#green").click(function(){
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+  $("button#yellow").click(function(){
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+  $("button#red").click(function(){
+    $("body").removeClass();
+    $("body").addClass("red-background");
   });
 });
