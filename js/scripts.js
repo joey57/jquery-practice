132672a6ed18fbb222175546cdab6e8db4c46067
joey57 <njorogejoey5@gmail.com>
2022-03-08 11:23:54 +0300

    added the ready function and $

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-jQuery('h1').click(function(){
+$(document).ready(function(){
+ $('h1').click(function(){
   alert("This is a header.");
   alert("I told you, THIS IS A HEADER!");
-});
-jQuery('P').click(function(){
+ });
+ $('P').click(function(){
   alert("This is paragraph")
-});
-jQuery('img').click(function(){
+ });
+ $('img').click(function(){
   alert("This is an image")
+ });
 });
