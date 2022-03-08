d05b3686573e48d241b72de7d4036a9177b345f1
joey57 <njorogejoey5@gmail.com>
2022-03-08 10:47:31 +0300

    changed jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-jQuery('h1').click(function(){
+jquery('h1').click(function(){
   alert("This is a heading tag")
 });
-jQuery('P').click(function(){
+jquery('P').click(function(){
   alert("This is paragraph")
 });
-jQuery('img').click(function(){
+jquery('img').click(function(){
   alert("This is an image")
 });
