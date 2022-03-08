f293ae17fe6e31b4409ed367f635a54e3b4bba39
joey57 <njorogejoey5@gmail.com>
2022-03-08 10:46:48 +0300

    added click function

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+jQuery('h1').click(function(){
+  alert("This is a heading tag")
+});
+jQuery('P').click(function(){
+  alert("This is paragraph")
+});
+jQuery('img').click(function(){
+  alert("This is an image")
+});
